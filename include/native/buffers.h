b40bb41575d828f5176ba448590782a11e10cb5e
Kyle Martin <kyle@kyleamartin.com>
2012-09-19 22:27:25 -0400

    Moving Buffer into native::detail

--- include/native/buffers.h
+++ include/native/buffers.h
@@ -5,87 +5,91 @@
 
 namespace native
 {
-    // TODO: implement Buffer class
-    class Buffer
-    {
-    public:
-        Buffer()
-            : data_()
-            , encoding_(detail::et_utf8)
-        {}
+	namespace detail {
 
-        Buffer(const Buffer& c)
-            : data_(c.data_)
-            , encoding_(c.encoding_)
-        {}
+	// TODO: implement Buffer class
+	class Buffer
+	{
+	public:
+		Buffer()
+			: data_()
+			, encoding_(detail::et_utf8)
+		{}
 
-        explicit Buffer(const std::vector<char>& data)
-            : data_(data)
-            , encoding_(detail::et_utf8)
-        {}
+		Buffer(const Buffer& c)
+			: data_(c.data_)
+			, encoding_(c.encoding_)
+		{}
 
-        explicit Buffer(const std::string& str)
-            : data_(str.begin(), str.end())
-            , encoding_(detail::et_ascii)
-        {}
+		explicit Buffer(const std::vector<char>& data)
+			: data_(data)
+			, encoding_(detail::et_utf8)
+		{}
 
-        explicit Buffer(const std::string& str, const std::string& encoding)
-            : data_(str.begin(), str.end())
-            , encoding_(detail::get_encoding_code(encoding))
-        {
-            // TODO: implement Buffer(const std::string& str, const std::string& encoding)
-            // ...
-        }
+		explicit Buffer(const std::string& str)
+			: data_(str.begin(), str.end())
+			, encoding_(detail::et_ascii)
+		{}
 
-        explicit Buffer(const char* data, std::size_t length)
-            : data_(data, data + length)
-            , encoding_(detail::et_utf8)
-        {}
+		explicit Buffer(const std::string& str, const std::string& encoding)
+			: data_(str.begin(), str.end())
+			, encoding_(detail::get_encoding_code(encoding))
+		{
+			// TODO: implement Buffer(const std::string& str, const std::string& encoding)
+			// ...
+		}
 
-        Buffer(Buffer&& c)
-            : data_(std::move(c.data_))
-            , encoding_(c.encoding_)
-        {}
+		explicit Buffer(const char* data, std::size_t length)
+			: data_(data, data + length)
+			, encoding_(detail::et_utf8)
+		{}
 
-        explicit Buffer(std::nullptr_t)
-            : data_()
-            , encoding_(detail::et_none)
-        {}
+		Buffer(Buffer&& c)
+			: data_(std::move(c.data_))
+			, encoding_(c.encoding_)
+		{}
 
-        virtual ~Buffer()
-        {}
+		explicit Buffer(std::nullptr_t)
+			: data_()
+			, encoding_(detail::et_none)
+		{}
 
-        char& operator[](std::size_t idx) { return data_[idx]; }
-        const char& operator[](std::size_t idx) const { return data_[idx]; }
+		virtual ~Buffer()
+		{}
 
-        Buffer& operator =(const Buffer& c)
-        {
-            data_ = c.data_;
-            encoding_ = c.encoding_;
-            return *this;
-        }
+		char& operator[](std::size_t idx) { return data_[idx]; }
+		const char& operator[](std::size_t idx) const { return data_[idx]; }
 
-        Buffer& operator =(Buffer&& c)
-        {
-            data_ = std::move(c.data_);
-            encoding_ = c.encoding_;
-            return *this;
-        }
+		Buffer& operator =(const Buffer& c)
+		{
+			data_ = c.data_;
+			encoding_ = c.encoding_;
+			return *this;
+		}
 
-        char* base() { return &data_[0]; }
-        const char* base() const { return &data_[0]; }
+		Buffer& operator =(Buffer&& c)
+		{
+			data_ = std::move(c.data_);
+			encoding_ = c.encoding_;
+			return *this;
+		}
 
-        std::size_t size() const { return data_.size(); }
+		char* base() { return &data_[0]; }
+		const char* base() const { return &data_[0]; }
 
-        Buffer slice(std::size_t start, std::size_t end)
-        {
-            return Buffer(std::vector<char>(data_.begin()+start, data_.begin()+end));
-        }
+		std::size_t size() const { return data_.size(); }
 
-    private:
-        std::vector<char> data_;
-        int encoding_;
-    };
-}
+		Buffer slice(std::size_t start, std::size_t end)
+		{
+			return Buffer(std::vector<char>(data_.begin()+start, data_.begin()+end));
+		}
+
+	private:
+		std::vector<char> data_;
+		int encoding_;
+	};
+
+	}  // namespace detail
+} // namespace native
 
 #endif
